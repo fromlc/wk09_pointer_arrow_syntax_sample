c074684e74716662624264fe2cdaa4fc23cfd71c
L Carver <wifihum@gmail.com>
2023-04-05 11:28:32 -0700

    Car.h is class declaration and definition file, remove unneeded Car.cpp, add EngineSpecs class to Car.h

--- source/Car.h
+++ source/Car.h
@@ -1,17 +1,53 @@
 //------------------------------------------------------------------------------
-// Car.h : class declaration file
+// Car.h : class declaration and definition file
 //------------------------------------------------------------------------------
 #pragma once
+
+#define DEBUG       // comment to disable verbose mode
+
+#ifdef DEBUG
+#include <iostream>
+using std::cout;
+#endif
 
 #include <string>
 using std::string;
 
+//------------------------------------------------------------------------------
+// EngineSpecs
+//------------------------------------------------------------------------------
+class EngineSpecs {
+public:
+    int redline;
+    int topspeed;
+
+    EngineSpecs() : redline(8000), topspeed(180) { }
+};
+
+//------------------------------------------------------------------------------
+// Engine
+//------------------------------------------------------------------------------
 class Engine {
 public:
     string eType;
+    EngineSpecs* pSpecs;
 
     // constructor
-    Engine(string _eType) : eType(_eType) { }
+    Engine(string _eType) { 
+
+        this->eType = _eType;
+        pSpecs = new EngineSpecs;
+    }
+
+    // destructor
+    ~Engine() {
+
+#ifdef DEBUG
+        cout << "Engine destructor: " << this->eType << '\n';
+#endif
+
+        delete pSpecs;
+    }
 };
 
 class Car {
@@ -20,14 +56,44 @@ public:
     string model;
     Engine* pEngine;
 
-    // constructors
-    Car();
-    Car(string _make, string _model, string _etype);
+    //------------------------------------------------------------------------------
+    // constructor
+    //------------------------------------------------------------------------------
+    Car() : Car("make", "model", "etype") { }
 
+    //------------------------------------------------------------------------------
+    // overload constructor copies string data
+    //------------------------------------------------------------------------------
+    Car(string _make, string _model, string _etype) :
+        make(_make), model(_model) {
+
+        this->pEngine = new Engine(_etype);
+    }
+
+    //------------------------------------------------------------------------------
     // destructor
-    ~Car();
+    //------------------------------------------------------------------------------
+    ~Car() {
 
-    string* getMake();
-    string* getModel();
-    string* getEngineType();
+#ifdef DEBUG
+        cout << "Car destructor: " << this->model << '\n';
+#endif
+
+        delete pEngine;
+    }
+
+    //------------------------------------------------------------------------------
+    // return pointer to make string
+    //------------------------------------------------------------------------------
+    string* getMake() { return &(this->make); }
+
+    //------------------------------------------------------------------------------
+    // return pointer to model string
+    //------------------------------------------------------------------------------
+    string* getModel() { return &(this->model); }
+
+    //------------------------------------------------------------------------------
+    // return pointer to Engine's etype string
+    //------------------------------------------------------------------------------
+    string* getEngineType() { return &(this->pEngine->eType); }
 };
