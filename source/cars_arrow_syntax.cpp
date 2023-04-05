428578c704a535f2e741db13b25672c53b6e4641
L Carver <wifihum@gmail.com>
2023-04-05 11:29:28 -0700

    add EngineSpecs display, output destructor invocation info

--- source/cars_arrow_syntax.cpp
+++ source/cars_arrow_syntax.cpp
@@ -16,21 +16,31 @@ using std::string;
 //------------------------------------------------------------------------------
 // local function prototypes
 //------------------------------------------------------------------------------
+void demoLocalCar();
 void displayCar(Car*);
+void displayEngineSpecs(Car*);
 
 //------------------------------------------------------------------------------
 // entry point
 //------------------------------------------------------------------------------
 int main() {
 
+	// causes destructor invocation
+	demoLocalCar();
+
 	Car* pVette = new Car("Chevy", "Corvette", "427 Big Block");
 	displayCar(pVette);
 
 	// must manually invoke destructor for memory allocated with new operator
+	cout << "freeing pVette memory with delete operator\n";
 	delete pVette;
+	pVette = nullptr;
 
-	Car vwGTI("Volkswagen", "Mk7 GTI", "2.0L TSI EA888 turbo");
-	displayCar(&vwGTI);
+	Car* pRx7 = new Car("Mazda", "Rx7", "1.3L Wankel Twin Turbo");
+	displayCar(pRx7);
+	displayEngineSpecs(pRx7);
+
+	cout << "did NOT delete pRx7 memory - memory leak!\n";
 
 	cout << "\nType any key to continue...";
 	cin.get();
@@ -39,11 +49,41 @@ int main() {
 }
 
 //------------------------------------------------------------------------------
+// local Car instance automatically invokes destructor on going out of scope
+//------------------------------------------------------------------------------
+void demoLocalCar() {
+
+	cout << "demoLocalCar():\n";
+
+	Car vwGTI("Volkswagen", "Mk7 GTI", "2.0L TSI EA888 turbo");
+	cout << "vwGTI is a local instance variable on the stack\n";
+	cout << "vwGTI memory will be deleted when demoLocalCar() returns\n";
+
+	// use dot syntax to access public instance data and functions
+	cout << "Engine type is " << *vwGTI.getEngineType() << '\n';
+
+	// pass address 
+	displayCar(&vwGTI);
+
+	cout << "demoLocalCar() returning to main()\n";
+}
+
+//------------------------------------------------------------------------------
 // display a Car's make, model, and engine type
 //------------------------------------------------------------------------------
 void displayCar(Car* pCar) {
 
-	cout << '\n' << *pCar->getMake() << ' '
+	cout << "\ndisplayCar(): " << *pCar->getMake() << ' '
 		<< *pCar->getModel() << ' '
 		<< *pCar->getEngineType() << '\n';
 }
+
+//------------------------------------------------------------------------------
+// display a Car's Egine's redline RPMs and top speed
+//------------------------------------------------------------------------------
+void displayEngineSpecs(Car* pCar) {
+
+	cout << "Top speed " << pCar->pEngine->pSpecs->topspeed << " MPH\n";
+	cout << "Redlines at " << pCar->pEngine->pSpecs->redline << "RPM\n";
+}
+
